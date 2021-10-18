908e0077ae2a1283f538cf9ce43c0db453754bd7
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2021-10-18 21:06:29 +0300

    урок 11

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,34 @@
 import React from 'react';
 import './App.css';
-import Header from './Header'
-import Telo from './Telo'
-import Footer from './Footer'
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Telo />
-      <Footer />
+    <div className='app-wrapper'>
+     <header className='header'>
+       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/24/LEGO_logo.svg/2048px-LEGO_logo.svg.png" alt=""/>
+     </header>
+      <nav className='nav'>
+        <a href="">Profile</a>
+        <a href="">Massages</a>
+        <a href="">News</a>
+        <a href="">Music</a>
+        <a href="">Setings</a>
+      </nav>
+      <div className='content'>
+        <img src="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270" alt=""/>
+
+        Main content
+
+        ava + discription
+
+        my posts
+
+        new post
+
+        post 1
+
+        post 2
+      </div>
     </div>
   );
 }
