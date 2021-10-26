21c1fc6311159a06e7b6ba4351f26105500f8b16
Evgeniy Sevostyanov <sevostyanov@aicrobotics.ru>
2021-10-26 20:35:33 +0300

    урок 18

--- my-app/src/components/Profile/Profile.jsx
+++ my-app/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import prof from './Profile.module.css';
 
 const Profile = () => {
   return (
-    <div className={prof.content}>
+    <div>
       <img src="https://images.ctfassets.net/hrltx12pl8hq/3MbF54EhWUhsXunc5Keueb/60774fbbff86e6bf6776f1e17a8016b4/04-nature_721703848.jpg?fit=fill&w=480&h=270" alt=""/>
 
       Main content
